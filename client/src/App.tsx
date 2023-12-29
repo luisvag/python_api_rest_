c96886ede80c4e19860436e0e8e464b9467d074e
Gabriel Marcano <gabrielmarcano120@gmail.com>
2023-12-28 22:48:49 -0400

    prettier + clean

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,15 @@
-import { Routes, Route } from "react-router-dom"
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-
-// export default function App() {
-//   return (
-//     <Container maxWidth="sm">
-//       <Box sx={{ my: 4 }}>
-//         {/* <Typography variant="h4" component="h1" gutterBottom>
-//           Material UI Vite.js example in TypeScript
-//         </Typography> */}
-//         {/* <ProTip /> */}
-//         <SignIn />
-//         {/* <Copyright /> */}
-//       </Box>
-//     </Container>
-//   );
-// }
+import { Routes, Route } from 'react-router-dom'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
 
 export default function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={ <SignIn /> } />
-        <Route path="sign-up" element={ <SignUp /> } />
-        {/* <Route path="reset-password" element={ <SignIn /> } /> */}
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<SignIn />} />
+                <Route path="sign-up" element={<SignUp />} />
+                {/* <Route path="reset-password" element={ <SignIn /> } /> */}
+            </Routes>
+        </div>
+    )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,15 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
-import theme from './theme';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import * as ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+import theme from './theme'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>,
-);
+    <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>
+)
