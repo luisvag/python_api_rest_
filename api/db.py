45522a4cc7812d90778be19512a27d6fbc88d7c2
luisvag <luisfranvag@gmail.com>
2024-01-03 22:47:12 -0400

    login

--- api/db.py
+++ api/db.py
@@ -1,4 +1,14 @@
 import pymysql
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+class User:
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+
+    def check_password(self, hashed_password, password):
+        return check_password_hash(hashed_password, password)
 
 
 def mysql():
